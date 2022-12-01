fc3225111f7d53196bdcc4fb745e0d9317f1e4ec
marchin7 <114770259+marchin7@users.noreply.github.com>
2022-12-01 14:07:53 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const inputs = document.querySelectorAll('input')
 const tipButtons = document.querySelectorAll('#btn');
 const billElement = document.getElementById('bill');
 const customElement = document.getElementById('custom');
@@ -10,10 +11,248 @@ const alertElement = document.getElementById('alert');
 let bill = 0;
 let tip = 0;
 let custom = 0;
-let people = 0;
+let people;
 let billPlusTip = 0;
 let tipAmountPerPerson = 0;
 let totalPerPerson = 0;
+let item = 0;
+
+function render(){
+    bill = Number(parseFloat(billElement.value))
+    people = Number((peopleElement.value));
+    tipAmountPerPerson = Math.round((billPlusTip / people)*100)/100;
+    totalPerPerson = Math.round(((billPlusTip - bill) / people)*100)/100;
+    totalElement.innerText = tipAmountPerPerson.toLocaleString("en-US", {style:"currency", currency:"USD"});
+    tipAmountElement.innerText = totalPerPerson.toLocaleString("en-US", {style:"currency", currency:"USD"})
+
+    if(tipAmountElement.innerText === '$NaN' || tipAmountElement.innerText === '$∞'){
+        tipAmountElement.innerText = '$0.00'
+    }
+    if(totalElement.innerText === '$NaN' || totalElement.innerText === '$∞'){
+        totalElement.innerText = '$0.00'
+    }
+}
+
+tipButtons.forEach(item =>{item.addEventListener('click', ()=>{
+    bill = Number(parseFloat(billElement.value))
+    tip = item.value
+    customElement.value = ''
+    billPlusTip = Math.round(((bill / 100 * tip) + bill)*100)/100;
+    console.log(item.value)
+    render()
+    alert()
+    
+    tipButtons.forEach(btn =>{
+        if(item !== btn) {
+          btn.classList.remove('active')
+        }
+        else {
+          btn.classList.add('active')
+        }
+      })
+      
+})})
+
+function customFunction(){
+    custom = Number(customElement.value);
+    people = Number(peopleElement.value);
+    billPlusTip = Math.round((bill + custom)*100)/100
+    render()
+    tipButtons.forEach(btn =>{
+        btn.classList.remove('active')
+    });
+}
+
+customElement.addEventListener('input', ()=>
+alert()
+)
+
+function peopleFunction(value) {
+    //customFunction() //
+    render()
+    alert()
+}
+
+function billFunction(value) {
+    render()
+    customFunction()
+}
+
+function alert() {
+    if(people === 0 || people === '') {
+
+        //customElement.value = ''
+        //peopleElement.focus()
+        peopleElement.style.outlineColor = 'rgb(212, 138, 0)'
+        alertElement.style.opacity ='1'
+    }
+    
+    else {
+        alertElement.style.opacity ='0'
+        peopleElement.style.outlineColor = 'hsl(172, 67%, 45%)'
+    }
+}
+
+resetBtn.addEventListener('click', ()=>{
+    billElement.value = ''
+    peopleElement.value = ''
+    customElement.value = ''
+    tipAmountPerPerson = 0;
+    totalPerPerson = 0;
+    totalElement.innerText = '$0.00'
+    tipAmountElement.innerText = '$0.00'
+    tipButtons.forEach(btn =>{
+        btn.classList.remove('active')
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function general(item){
+    bill = Number(billElement.value)
+    tip = Number(item.value);
+    custom = Number(customElement.value);
+    people = Number(peopleElement.value);
+
+    customElement.value = ''
+
+    billPlusTip = Math.round(((bill / 100 * tip) + bill)*100)/100;
+    tipAmountPerPerson = Math.round((billPlusTip / people)*100)/100;
+    totalPerPerson = Math.round(((billPlusTip - bill) / people)*100)/100;
+
+    totalElement.innerText = tipAmountPerPerson.toLocaleString("en-US", {style:"currency", currency:"USD"});
+    tipAmountElement.innerText = totalPerPerson.toLocaleString("en-US", {style:"currency", currency:"USD"})
+}
+
+tipButtons.forEach(item =>{item.addEventListener('click', btnFunction)
+
+
+
+function btnFunction() {
+
+    general(item)
+
+    tipButtons.forEach(btn =>{
+
+    if(item !== btn) {
+      btn.classList.remove('active')
+    }
+    else {
+      btn.classList.add('active')
+    }
+  })
+}
+
+
+})
+
+
+
+
+
+
+customElement.addEventListener('input', customFunction)
+
+
+function customFunction(){
+
+    tipButtons.forEach(btn=>{
+        btn.classList.remove('active')
+    })
+
+    bill = Number(billElement.value)
+    tip = Number(tipAmountElement.value);
+    custom = Number(customElement.value);
+    people = Number(peopleElement.value);
+
+    billPlusTip = Math.round((bill + custom)*100)/100
+    tipAmountPerPerson = Math.round((billPlusTip / people)*100)/100
+    totalPerPerson = Math.round(((billPlusTip - bill) / people)*100)/100
+    
+    totalElement.innerText = tipAmountPerPerson.toLocaleString("en-US", {style:"currency", currency:"USD"});
+    tipAmountElement.innerText = totalPerPerson.toLocaleString("en-US", {style:"currency", currency:"USD"});
+}
+
+
+
+function peopleFunction(item) {
+    if (tip){
+        btnFunction(item)
+    }
+
+}
+
+
+
+*/
+
+
+
+
+
+
+  
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 
 tipButtons.forEach(btn=>{btn.addEventListener('click', btnResult)
@@ -77,7 +316,7 @@ customElement.addEventListener('input', ()=>{
         peopleElement.style.outlineColor = 'hsl(172, 67%, 45%)'
     }
  
-    billPlusTip = Math.round(((bill / 100 * custom) + bill)*100)/100;
+    billPlusTip = Math.round((bill + custom) *100)/100;
     tipAmountPerPerson = Math.round((billPlusTip / people)*100)/100;
     totalPerPerson = Math.round(((billPlusTip - bill) / people)*100)/100;
     let tipAmountPerPersonToLocale = tipAmountPerPerson.toLocaleString("en-US", {style:"currency", currency:"USD"});
@@ -109,5 +348,5 @@ resetBtn.addEventListener('click', ()=>{
     totalElement.innerText = '$0.00'
     tipAmountElement.innerText = '$0.00'
 })
-    
 
+*/
